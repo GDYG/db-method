60ea83eefa1ffe92c8e2cb0e2633c2005cb47017
YAHUI AN <1549472324@qq.com>
2020-06-10 22:26:04 +0800

    add Method

--- index.js
+++ index.js
@@ -21,7 +21,52 @@ function arrayFlat(arr) {
     }
 }
 
-export default {
-    arraySet: arraySet,
-    arrayFlat: arrayFlat
+//函数防抖
+function debounce(fn) {
+    var timer = null;
+    return function() {
+        var that = this;
+        var args = arguments;
+        if(timer) clearTimeout(timer);
+        timer = setTimeout(function () {
+            fn.apply(that, args)
+        }, 4000)
+    }
 }
+
+//函数节流
+function throttle(fn) {  
+    var timer = new Date().getTime();
+    return function() {
+        var that = this;
+        var args = arguments;
+        var nowTime = Date.now();
+        if(nowTime - timer > 3000) {
+            fn.apply(that, args);
+            timer = nowTime;
+        }
+    } 
+}
+
+//冒泡排序
+function sortMao(arr) {  
+    var len = arr.length;
+    for(var i=0;i<len-1;i++) {
+        for(var j=0;j<len-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                var arrItem = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = arrItem;
+            }
+        }
+    }
+    return arr;
+}
+
+
+// export default {
+//     arraySet: arraySet,
+//     arrayFlat: arrayFlat,
+//     debounce: debounce,
+//     throttle: throttle
+// }
